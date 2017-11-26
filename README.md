ce0a74c1bc4eea76c5a812ae970f4d0e98e6ea54
Ian Ibbotson <ianibbo@gmail.com>
2017-11-26 12:27:58 +0000

    Researching sparql to extract monitoring locations

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://react-md.mlaursen.com/getting-started/installation
 
 https://react-md.mlaursen.com/
 https://tomchentw.github.io/react-google-maps/
+
+http://demo.semweb.co/sparql?default-graph-uri=&query=select+%3Fs+%3Flat+%3Flon+%3Fid+where+%7B%0D%0A++%3Fs+a+%3Chttp%3A%2F%2Fpurl.oclc.org%2FNET%2Fssnx%2Fssn%23SensingDevice%3E+.%0D%0A++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23lat%3E+%3Flat+.%0D%0A++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23long%3E+%3Flon+.%0D%0A++%3Fs+%3Curi%3A%2F%2Fopensheffield.org%2Fproperties%23sensorId%3E+%3Fid+.%0D%0A++FILTER%28NOT+EXISTS+%7B+%3Fs+a+%3Curi%3A%2F%2Fopensheffield.org%2Ftypes%23diffusionTube%3E+%7D+%29%0D%0A%7D%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on
--- aq-map/src/index.js
+++ aq-map/src/index.js
@@ -8,6 +8,24 @@ import './index.css'
 
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+//
+// Some sparql examples here
+// https://github.com/BetterWithDataSociety/OpenDataKiosk/blob/dev/AirMap2/partial/AirMap2/AirMap2.js
+//
+//  
+//  $http.get("http://apps.opensheffield.org/sparql?default-graph-uri=&query=select+%3Fs+%3Flat+%3Flon+%3Fid+where+%7B%0D%0A++%3Fs+a+%3Chttp%3A%2F%2Fpurl.oclc.org%2FNET%2Fssnx%2Fssn%23SensingDevice%3E+.%0D%0A++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23lat%3E+%3Flat+.%0D%0A++%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23long%3E+%3Flon+.%0D%0A++%3Fs+%3Curi%3A%2F%2Fopensheffield.org%2Fproperties%23sensorId%3E+%3Fid+.%0D%0A++FILTER%28NOT+EXISTS+%7B+%3Fs+a+%3Curi%3A%2F%2Fopensheffield.org%2Ftypes%23diffusionTube%3E+%7D+%29%0D%0A%7D%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on").success( function(data) {
+//            for ( var i = 0; i < data.results.bindings.length; i++ ) {
+//                    var p = new ol.geom.Point(ol.proj.transform([parseFloat(data.results.bindings[i].lon.value), 
+//                                                                         parseFloat(data.results.bindings[i].lat.value)], 'EPSG:4326', 'EPSG:900913'));
+//
+//                                                                                 var f = new ol.Feature({geometry:p,
+//                                                                                                                 type : 'RTMonitoring',
+//                                                                                                                                                 uri : data.results.bindings[i].s.value,
+//                                                                                                                                                                                 label : data.results.bindings[i].id.value});
+//                                                                                                                                                                                         
+//                                                                                                                                                                                                 f.setStyle(rtStyle);
+//                                                                                                                                                                                                         mkrs.addFeatures([f]);
+//  }
 
 
 export default class extends Component {
